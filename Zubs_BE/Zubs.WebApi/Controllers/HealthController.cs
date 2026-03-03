26d1d947c7fbe55bbacea40072185d3f26ee216a
Ivan Zubets <onezub2008@gmail.com>
2026-03-03 09:30:18 +0200

    Adding HealthController

---
+++ Zubs_BE/Zubs.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zubs.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public ActionResult Health()
+        {
+            return Ok("Healthy");
+        }
+
+    }
+}
